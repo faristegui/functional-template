a8de943e55262c2ef5a56e80ea06b8ebb7da612a
Facundo Aristegui <faristegui@fi.uba.ar>
2018-09-07 10:07:57 -0300

    Arreglado Test ejercicio 7

--- test/exercise7_test.clj
+++ test/exercise7_test.clj
@@ -4,11 +4,15 @@
 
 (deftest enesimo-elemento-vector-numerico
   (testing "Devuelve el enesimo elemento numérico."
-    (is (= (enesimo 3 [1 2 3 4 5]) 4))))
+    (is (= (enesimo 1 [1 2 3 4]) 2))))
 
 (deftest enesimo-elemento-vector-string
   (testing "Devuelve el enesimo elemento de un vector de string."
     (is (= (enesimo 0 ["FIUBA" "TDD" "7510"]) "FIUBA"))))
+	
+(deftest enesimo-elemento-mapa
+  (testing "Devuelve el enesimo elemento de un mapa."
+    (is (= (enesimo 0 [:FIUBA :TDD :7510]) :FIUBA))))
 
 (deftest enesimo-lista-strings
   (testing "Devuelve enesimo elemento de una lista de strings"
@@ -16,4 +20,8 @@
 
 (deftest enesimo-lista-numeros
   (testing "Devuelve enesimo elemento de una lista numerica."
-	(is (= (enesimo 4 '(1 2 3 4 5 10 22)) 5))))
+	(is (= (enesimo 2 '(4 5 6 7)) 6))))
+	
+	(deftest enesimo-lista-vectores
+  (testing "Devuelve enesimo elemento de una lista de vectores."
+	(is (= (enesimo 2 '([1 2] [3 4] [5 6])) [5 6]))))
