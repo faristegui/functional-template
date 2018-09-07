8024396aacc78f3c19cebd2f492584f379dc87f1
Facundo Aristegui <faristegui@fi.uba.ar>
2018-09-07 10:19:05 -0300

    Subido test ejercicio 8

---
+++ test/exercise8_test.clj
@@ -0,0 +1,21 @@
+(ns exercise8-test
+  (:require [clojure.test :refer :all]
+            [exercise8 :refer :all]))
+
+(deftest separador-vector-numerico
+  (testing "Separa el vector numerico en la posicion pasada por parametro"
+    (is (= (separador 3 [1 2 3 4 5 6]) ['(1 2 3) '(4 5 6)]))
+  )
+)
+
+(deftest separador-vector-mapas
+  (testing "Separa el vector de mapas en la posicion pasada por parametro"
+    (is (= (separador 1 [:a :b :c :d]) ['(:a) '(:b :c :d)]))
+   )
+)
+
+(deftest separador-vector-de-vectores
+  (testing "Devuelve enesimo elemento de una lista de strings"
+    (is (= (separador 2 [[1 2] [3 4] [5 6]]) ['([1 2] [3 4]) '([5 6])])
+  )
+)
